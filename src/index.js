e96c9fa53352231163693a450026f157a405a522
Kimberly Branch <kbranch@enova.com>
2020-08-22 10:41:24 -0500

    step-2: fix to pass test

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const User=require('./models/User');
 app.listen(3000, ()=>{
 })
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+//app.get('/', function (req, res) {
+//  res.send('Hello World!')
+//})
 
 app.route('/users')
 // READ
