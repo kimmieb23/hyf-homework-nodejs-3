1069906fc18e1e3c8437002682f1f2d7e1e9fc8a
Kimberly Branch <kbranch@enova.com>
2020-08-22 09:35:53 -0500

    step-1 #2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express= require('express');
 const app= express();
 
+const User=require('./models/User');
+
 app.listen(3000, ()=>{
 })
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.route('/users')
+// READ
+.get((req,res)=>{
+  // User.findById()
+	)
 })
